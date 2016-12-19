ae4ce3549bcbcba0a42f5d017c39c5752bf3190d
gurleensethi <sarusethi@rocketmail.com>
2016-12-19 20:13:40 +0530

    #Update : Item UI

--- app/src/main/java/app/com/thetechnocafe/quicknewsbytes/Networking/NetworkRequests.java
+++ app/src/main/java/app/com/thetechnocafe/quicknewsbytes/Networking/NetworkRequests.java
@@ -118,7 +118,7 @@ public class NetworkRequests {
                         }
                     }
                 } catch (JSONException e) {
-                    e.printStackTrace();
+
                 }
             }
         }, new Response.ErrorListener() {
